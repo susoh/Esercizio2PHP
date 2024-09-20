2ea83141ffc6a71de07226cf345836f836cc1253
Davide Susini <susini.davide@itismeucci.com>
2024-09-20 10:43:37 +0200

    aggiunta funzione per decimali

--- EsercizioCapitale_Susini.php
+++ EsercizioCapitale_Susini.php
@@ -14,6 +14,7 @@
         echo "<p> Il tasso è: $tasso </p><br>";
         echo "<p> Il numero di giorni è: $numGiorni </p><br>";
         $interesse = ($capitale * $tasso * $numGiorni) /36500;
+        number_format($interesse, 2);
         echo "<h2 style='color: green'> Il tasso di interesse è: $interesse €</h2>"
     ?>
 </body>
