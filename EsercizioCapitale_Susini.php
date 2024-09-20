24f699f6cfd452555fe0e1427fb1047b64ad3983
Davide Susini <susini.davide@itismeucci.com>
2024-09-20 10:05:17 +0200

    esercizio 2 completato

---
+++ EsercizioCapitale_Susini.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $capitale = 1000;
+        $tasso = 5;
+        $numGiorni = 200;
+        echo "<p> Il capitale è: $capitale </p><br>";
+        echo "<p> Il tasso è: $tasso </p><br>";
+        echo "<p> Il numero di giorni è: $numGiorni </p><br>";
+        $interesse = ($capitale * $tasso * $numGiorni) /36500;
+        echo "<h2 style='color: green'> Il tasso di interesse è: $interesse €</h2>"
+    ?>
+</body>
+</html>
